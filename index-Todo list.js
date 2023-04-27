c11aad7f83c1c219bb48d43768b1aee0c0924222
Orekoya <tundeorekoya1998@gmail.com>
2023-04-28 00:10:47 +0100

    new  file

---
+++ index-Todo list.js
@@ -0,0 +1,61 @@
+
+todoList = [{
+  name:'',
+  dueDate:''
+},
+  {name: '',
+   dueDate:''
+  }] 
+
+renderTodolist();
+
+function renderTodolist() {
+
+
+  let todoListHTML = '';
+
+  for (let i = 0; i < todoList.length; i++ ) {
+      const todoObject = todoList[i];
+      const {name,dueDate }  = todoObject;
+      
+
+      const html =  `<div>${name}</div> 
+                      <div>${dueDate}</div> 
+      <button onclick = 
+      " todoList.splice(${i}, 1);
+      renderTodolist();
+      "class="delete-button">Delete
+      </button>`;
+  
+      todoListHTML += html; 
+
+    
+  document.querySelector(`.js-todo-List`)
+  .innerHTML = todoListHTML;
+  }
+  
+}
+
+
+
+function addTodo (){
+  const inputElement = document.querySelector(`.js-name-input`);
+
+   const name = inputElement.value;
+
+   const dateInputElement = document.querySelector('.js-due-date-input')
+
+   const dueDate = dateInputElement.value;
+
+   todoList.push( {
+    name:name,
+    dueDate:dueDate})
+
+
+
+
+   inputElement.value = '';
+
+   renderTodolist();
+}
+
